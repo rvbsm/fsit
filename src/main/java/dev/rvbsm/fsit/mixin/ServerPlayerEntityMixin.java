5f6145daaaa90d31e33d63938a4e404b0f0dbffa
Ruslan Mykhailenko <mihailencko.r222@gmail.com>
2023-03-15 21:24:55 +0200

    feat: 1.19.4 compatibility

--- src/main/java/dev/rvbsm/fsit/mixin/ServerPlayerEntityMixin.java
+++ src/main/java/dev/rvbsm/fsit/mixin/ServerPlayerEntityMixin.java
@@ -5,7 +5,6 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.network.ServerPlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -13,11 +12,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(ServerPlayerEntity.class)
 public abstract class ServerPlayerEntityMixin {
 
-	@Shadow public abstract void stopRiding();
-
 	@Inject(at = @At(value = "HEAD"), method = "onDisconnect")
 	public void onDisconnect(CallbackInfo ci) {
-		final Entity vehicle = ((PlayerEntity) (Object) this).getVehicle();
-		if (vehicle instanceof SeatEntity) this.stopRiding();
+		final PlayerEntity player = (PlayerEntity) (Object) this;
+		final Entity vehicle = player.getVehicle();
+		if (vehicle instanceof SeatEntity) player.stopRiding();
 	}
 }
