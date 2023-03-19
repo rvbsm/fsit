325631e46b03688f5c732108605dedfe7b22304f
Ruslan Mykhailenko <mihailencko.r222@gmail.com>
2023-03-19 15:00:30 +0200

    fix: remove passenger when player changed gamemode to spectator

--- src/main/java/dev/rvbsm/fsit/mixin/ServerPlayerEntityMixin.java
+++ src/main/java/dev/rvbsm/fsit/mixin/ServerPlayerEntityMixin.java
@@ -4,18 +4,26 @@ import dev.rvbsm.fsit.entity.SeatEntity;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.world.GameMode;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ServerPlayerEntity.class)
 public abstract class ServerPlayerEntityMixin {
 
 	@Inject(method = "onDisconnect", at = @At(value = "HEAD"))
 	public void onDisconnect(CallbackInfo ci) {
-		final PlayerEntity player = (PlayerEntity) (Object) this;
+		final ServerPlayerEntity player = (ServerPlayerEntity) (Object) this;
 		final Entity vehicle = player.getVehicle();
 		if (vehicle instanceof SeatEntity) player.stopRiding();
+	}
+
+	@Inject(method = "changeGameMode", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/network/ServerPlayerEntity;stopRiding()V"))
+	public void changeGameMode(GameMode gameMode, CallbackInfoReturnable<Boolean> cir) {
+		final PlayerEntity player = (ServerPlayerEntity) (Object) this;
+		if (player.getFirstPassenger() instanceof PlayerEntity) player.getFirstPassenger().stopRiding();
 	}
 }
