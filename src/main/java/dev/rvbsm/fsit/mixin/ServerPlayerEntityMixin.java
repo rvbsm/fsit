9c96152c4690b0d5d940f9009ce0a086dd3023cd
Ruslan Mykhailenko <mihailencko.r222@gmail.com>
2023-03-20 18:41:21 +0200

    fix: player is already stop riding at `changeGameMode` to `SPECTATOR`

--- src/main/java/dev/rvbsm/fsit/mixin/ServerPlayerEntityMixin.java
+++ src/main/java/dev/rvbsm/fsit/mixin/ServerPlayerEntityMixin.java
@@ -2,14 +2,11 @@ package dev.rvbsm.fsit.mixin;
 
 import dev.rvbsm.fsit.entity.SeatEntity;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.world.GameMode;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ServerPlayerEntity.class)
 public abstract class ServerPlayerEntityMixin {
@@ -19,11 +16,5 @@ public abstract class ServerPlayerEntityMixin {
 		final ServerPlayerEntity player = (ServerPlayerEntity) (Object) this;
 		final Entity vehicle = player.getVehicle();
 		if (vehicle instanceof SeatEntity) player.stopRiding();
-	}
-
-	@Inject(method = "changeGameMode", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/network/ServerPlayerEntity;stopRiding()V"))
-	public void changeGameMode(GameMode gameMode, CallbackInfoReturnable<Boolean> cir) {
-		final PlayerEntity player = (ServerPlayerEntity) (Object) this;
-		if (player.getFirstPassenger() instanceof PlayerEntity) player.getFirstPassenger().stopRiding();
 	}
 }
