9b7e0ddce45e554ecacf2e24a1d2157da4ec0cf8
Ruslan Mykhailenko <mihailencko.r222@gmail.com>
2023-03-22 05:20:48 +0200

    revert: `/fsit reload` command is WIP

--- src/main/java/dev/rvbsm/fsit/FSitServerMod.java
+++ src/main/java/dev/rvbsm/fsit/FSitServerMod.java
@@ -5,7 +5,7 @@ import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
-import dev.rvbsm.fsit.config.FSitConfigManager;
+//import dev.rvbsm.fsit.config.FSitConfigManager;
 import dev.rvbsm.fsit.entity.SeatEntity;
 import net.fabricmc.api.DedicatedServerModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
@@ -23,18 +23,18 @@ public class FSitServerMod implements DedicatedServerModInitializer {
 	@Override
 	public void onInitializeServer() {
 		CommandRegistrationCallback.EVENT.register(this::registerSitCommand);
-		CommandRegistrationCallback.EVENT.register(this::registerReloadCommand);
+//		CommandRegistrationCallback.EVENT.register(this::registerReloadCommand);
 	}
 
 	private void registerSitCommand(@NotNull CommandDispatcher<ServerCommandSource> dispatcher, CommandRegistryAccess registryAccess, CommandManager.RegistrationEnvironment environment) {
 		dispatcher.register(LiteralArgumentBuilder.<ServerCommandSource>literal("sit").executes(this::sitCommand));
 	}
 
-	private void registerReloadCommand(@NotNull CommandDispatcher<ServerCommandSource> dispatcher, CommandRegistryAccess registryAccess, CommandManager.RegistrationEnvironment environment) {
-		dispatcher.register(LiteralArgumentBuilder.<ServerCommandSource>literal(FSitMod.getModId())
-						.then(LiteralArgumentBuilder.literal("reload"))
-						.executes(this::reloadCommand));
-	}
+//	private void registerReloadCommand(@NotNull CommandDispatcher<ServerCommandSource> dispatcher, CommandRegistryAccess registryAccess, CommandManager.RegistrationEnvironment environment) {
+//		dispatcher.register(LiteralArgumentBuilder.<ServerCommandSource>literal(FSitMod.getModId())
+//						.then(LiteralArgumentBuilder.literal("reload"))
+//						.executes(this::reloadCommand));
+//	}
 
 	private int sitCommand(@NotNull CommandContext<ServerCommandSource> ctx) {
 		final ServerCommandSource source = ctx.getSource();
@@ -55,9 +55,9 @@ public class FSitServerMod implements DedicatedServerModInitializer {
 		return Command.SINGLE_SUCCESS;
 	}
 
-	private int reloadCommand(@NotNull CommandContext<ServerCommandSource> ctx) {
-		FSitConfigManager.load();
-		ctx.getSource().sendFeedback(Text.literal("Reload config"), false);
-		return Command.SINGLE_SUCCESS;
-	}
+//	private int reloadCommand(@NotNull CommandContext<ServerCommandSource> ctx) {
+//		FSitConfigManager.load();
+//		ctx.getSource().sendFeedback(Text.literal("Reload config"), false);
+//		return Command.SINGLE_SUCCESS;
+//	}
 }
