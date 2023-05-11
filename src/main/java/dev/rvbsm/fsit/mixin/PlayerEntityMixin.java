e66fdf138388014afaa25f0b9fb96c86f449c9cd
Ruslan Mykhailenko <mihailencko.r222@gmail.com>
2023-05-11 18:52:20 +0300

    refactor(mixin): set environment server

--- src/main/java/dev/rvbsm/fsit/mixin/PlayerEntityMixin.java
+++ src/main/java/dev/rvbsm/fsit/mixin/PlayerEntityMixin.java
@@ -1,6 +1,8 @@
 package dev.rvbsm.fsit.mixin;
 
 import dev.rvbsm.fsit.entity.SeatEntity;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.minecraft.entity.player.PlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -8,12 +10,12 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(PlayerEntity.class)
-public class PlayerEntityMixin {
+public abstract class PlayerEntityMixin {
 
+	@Environment(EnvType.SERVER)
 	@Inject(method = "shouldDismount", at = @At(value = "HEAD"), cancellable = true)
 	protected void shouldDismount(CallbackInfoReturnable<Boolean> cir) {
 		final PlayerEntity player = (PlayerEntity) (Object) this;
-		if (player.world.isClient) return;
 
 		// ! bruh
 		if (player.getVehicle() instanceof final SeatEntity seatEntity) if (seatEntity.age <= 10) cir.setReturnValue(false);
