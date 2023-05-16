e4bc8956448105c37213e3a35827abc20410b7e0
Ruslan Mykhailenko <mihailencko.r222@gmail.com>
2023-05-16 18:46:16 +0300

    refactor(event): update sit-on-player conditions

--- src/main/java/dev/rvbsm/fsit/event/InteractPlayerCallback.java
+++ src/main/java/dev/rvbsm/fsit/event/InteractPlayerCallback.java
@@ -1,9 +1,10 @@
 package dev.rvbsm.fsit.event;
 
 import dev.rvbsm.fsit.config.FSitConfig;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.HitResult;
@@ -11,14 +12,15 @@ import net.minecraft.world.World;
 
 public abstract class InteractPlayerCallback {
 
-	public static ActionResult interactPlayer(PlayerEntity player, World world, Hand hand, Entity entity, HitResult ignored) {
+	@Environment(EnvType.SERVER)
+	public static ActionResult interactPlayer(PlayerEntity player, World ignored, Hand hand, Entity entity, HitResult ignored1) {
 		if (!FSitConfig.sitPlayers.getValue()) return ActionResult.PASS;
-		else if (world.isClient || player.isSpectator()) return ActionResult.PASS;
-		else if (player.shouldCancelInteraction()) return ActionResult.PASS;
+		else if (player.isSpectator() || entity.isSpectator()) return ActionResult.PASS;
+		else if (player.isSneaking() || entity.isSneaking()) return ActionResult.PASS;
 		else if (hand != Hand.MAIN_HAND) return ActionResult.PASS;
 		else if (!player.getStackInHand(hand).isEmpty()) return ActionResult.PASS;
 
-		if (entity instanceof ServerPlayerEntity) if (!entity.isSpectator() && !entity.hasPassengers()) {
+		if (entity instanceof PlayerEntity && !entity.hasPassengers()) {
 			player.startRiding(entity, true);
 
 			return ActionResult.SUCCESS;
