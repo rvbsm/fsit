f63a9542d941f6e523d38e1cc8adc2205be56b94
Ruslan Mykhailenko <mihailencko.r222@gmail.com>
2023-07-08 17:38:06 +0300

    feat(config): remove comments while migrate
    rename method with more precise name
    make class package-private

--- src/main/java/dev/rvbsm/fsit/config/ConfigMigrator.java
+++ src/main/java/dev/rvbsm/fsit/config/ConfigMigrator.java
@@ -1,22 +1,25 @@
 package dev.rvbsm.fsit.config;
 
-public abstract class ConfigMigrator {
+abstract class ConfigMigrator {
 
-	protected static void migrate2To3() {
-		FSitConfig.config.set("config_version", 3);
+	protected static void migrateFrom2() {
+		FSitConfig.config.set("config_version", ConfigData.Entries.CONFIG_VERSION.defaultValue());
 
 		if (FSitConfig.config.contains("sneak_sit")) {
 			final boolean sneak_sit = FSitConfig.config.remove("sneak_sit");
+			FSitConfig.config.removeComment("sneak_sit");
 			FSitConfig.config.set(ConfigData.Fields.SNEAK_ENABLED, sneak_sit);
 		}
 
 		if (FSitConfig.config.contains("sneak.sneak_delay")) {
 			final int sneak_delay = FSitConfig.config.remove("sneak.sneak_delay");
+			FSitConfig.config.removeComment("sneak.sneak_delay");
 			FSitConfig.config.set(ConfigData.Fields.SNEAK_DELAY, sneak_delay);
 		}
 
 		if (FSitConfig.config.contains("misc.sit_players")) {
 			final boolean ride_players = FSitConfig.config.remove("misc.sit_players");
+			FSitConfig.config.removeComment("misc.sit_players");
 			FSitConfig.config.set(ConfigData.Fields.RIDE_PLAYERS, ride_players);
 		}
 	}
