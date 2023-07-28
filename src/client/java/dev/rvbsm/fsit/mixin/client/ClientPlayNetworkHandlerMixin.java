d27c8ada6d4432cf0671f1cc0483082edd9bd21d
Ruslan Mykhailenko <mihailencko.r222@gmail.com>
2023-07-28 23:49:35 +0300

    fix(client): doubling on pose message

--- src/client/java/dev/rvbsm/fsit/mixin/client/ClientPlayNetworkHandlerMixin.java
+++ src/client/java/dev/rvbsm/fsit/mixin/client/ClientPlayNetworkHandlerMixin.java
@@ -1,10 +1,8 @@
 package dev.rvbsm.fsit.mixin.client;
 
 import dev.rvbsm.fsit.FSitMod;
-import dev.rvbsm.fsit.entity.PlayerPoseAccessor;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
-import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.entity.Entity;
 import net.minecraft.network.packet.s2c.play.EntityPassengersSetS2CPacket;
 import net.minecraft.text.Text;
@@ -12,9 +10,7 @@ import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
@@ -26,19 +22,11 @@ public abstract class ClientPlayNetworkHandlerMixin {
 	private MinecraftClient client;
 
 	@Inject(method = "onEntityPassengersSet", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;startRiding(Lnet/minecraft/entity/Entity;Z)Z"), locals = LocalCapture.CAPTURE_FAILSOFT)
-	public void onEntityPassengersSet$startRiding(EntityPassengersSetS2CPacket packet, CallbackInfo ci, Entity entity, boolean bl, int[] var4, int var5, int var6, int i, Entity entity2) {
-		if (entity == this.client.player) {
+	public void onEntityPassengersSet$startRiding(EntityPassengersSetS2CPacket packet, CallbackInfo ci, Entity passenger, boolean bl, int[] var4, int var5, int var6, int i, Entity vehicle) {
+		if (passenger == this.client.player && vehicle.isPlayer()) {
 			final Text text = FSitMod.getTranslation("message", "onride", this.client.options.sneakKey.getBoundKeyLocalizedText());
 			this.client.inGameHud.setOverlayMessage(text, false);
 			this.client.getNarratorManager().narrate(text);
 		}
-	}
-
-	@ModifyConstant(method = "onEntityPassengersSet", constant = @Constant(stringValue = "mount.onboard"))
-	private String onEntityPassengersSet$key(String constant) {
-		final ClientPlayerEntity player;
-		return (player = this.client.player) != null && ((PlayerPoseAccessor) player).isPosing()
-						? FSitMod.getTranslationKey("message", "onpose")
-						: constant;
 	}
 }
