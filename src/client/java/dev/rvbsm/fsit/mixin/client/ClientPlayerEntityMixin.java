19910f796735444fec73c4bb886288029facc39e
Ruslan Mykhailenko <mihailencko.r222@gmail.com>
2023-10-05 13:32:18 +0300

    fix(config): implement restriction update for `ClientPlayerEntity` (with f4a733a2)

--- src/client/java/dev/rvbsm/fsit/mixin/client/ClientPlayerEntityMixin.java
+++ src/client/java/dev/rvbsm/fsit/mixin/client/ClientPlayerEntityMixin.java
@@ -3,6 +3,8 @@ package dev.rvbsm.fsit.mixin.client;
 import dev.rvbsm.fsit.FSitMod;
 import dev.rvbsm.fsit.entity.PlayerPose;
 import dev.rvbsm.fsit.mixin.PlayerEntityMixin;
+import dev.rvbsm.fsit.network.packet.RestrictPlayerC2SPacket;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.entity.EntityType;
@@ -11,6 +13,8 @@ import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+
+import java.util.UUID;
 
 @Mixin(ClientPlayerEntity.class)
 public abstract class ClientPlayerEntityMixin extends PlayerEntityMixin {
@@ -29,5 +33,19 @@ public abstract class ClientPlayerEntityMixin extends PlayerEntityMixin {
 
 		if (this.isPosing() && !this.isInPose(PlayerPose.SIT))
 			this.client.inGameHud.setOverlayMessage(FSitMod.getTranslation("message", "onpose", this.client.options.sneakKey.getBoundKeyLocalizedText()), false);
+	}
+
+	@Override
+	public void fsit$restrictPlayer(UUID playerUUID) {
+		super.fsit$restrictPlayer(playerUUID);
+
+		ClientPlayNetworking.send(new RestrictPlayerC2SPacket(playerUUID, RestrictPlayerC2SPacket.Type.RESTRICT));
+	}
+
+	@Override
+	public void fsit$allowPlayer(UUID playerUUID) {
+		super.fsit$allowPlayer(playerUUID);
+
+		ClientPlayNetworking.send(new RestrictPlayerC2SPacket(playerUUID, RestrictPlayerC2SPacket.Type.ALLOW));
 	}
 }
