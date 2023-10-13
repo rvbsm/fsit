942748ad1241de5a8319c55bae77d388c12ef681
Ruslan Mykhailenko <mihailencko.r222@gmail.com>
2023-10-13 11:54:41 +0300

    fix(client): config isn't sent to server. #23

--- src/client/java/dev/rvbsm/fsit/network/ClientNetworkHandler.java
+++ src/client/java/dev/rvbsm/fsit/network/ClientNetworkHandler.java
@@ -1,7 +1,10 @@
 package dev.rvbsm.fsit.network;
 
+import dev.rvbsm.fsit.FSitMod;
+import dev.rvbsm.fsit.FSitModClient;
 import dev.rvbsm.fsit.entity.PoseHandler;
 import dev.rvbsm.fsit.entity.RestrictHandler;
+import dev.rvbsm.fsit.network.packet.ConfigSyncC2SPacket;
 import dev.rvbsm.fsit.network.packet.PoseSyncS2CPacket;
 import dev.rvbsm.fsit.network.packet.RestrictionListSyncS2CPacket;
 import net.fabricmc.fabric.api.networking.v1.PacketSender;
@@ -9,11 +12,13 @@ import net.minecraft.client.network.ClientPlayerEntity;
 
 public class ClientNetworkHandler {
 
+	public static void receiveRestrictionList(RestrictionListSyncS2CPacket packet, ClientPlayerEntity player, PacketSender sender) {
+		FSitModClient.setConnected(true);
+
+		sender.sendPacket(new ConfigSyncC2SPacket(FSitMod.getConfig()));
+		packet.restrictList().forEach(((RestrictHandler) player)::fsit$restrictPlayer);
+	}
 	public static void receivePoseSync(PoseSyncS2CPacket packet, ClientPlayerEntity player, PacketSender sender) {
 		((PoseHandler) player).fsit$setPose(packet.pose());
-	}
-
-	public static void receiveRestrictionList(RestrictionListSyncS2CPacket packet, ClientPlayerEntity player, PacketSender sender) {
-		packet.restrictList().forEach(((RestrictHandler) player)::fsit$restrictPlayer);
 	}
 }
