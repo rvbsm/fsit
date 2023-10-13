11e631531f3d02fd46c8a1e0375f35732211fbef
Ruslan Mykhailenko <mihailencko.r222@gmail.com>
2023-10-13 11:54:02 +0300

    fix(client): Minecraft::getServer returns integrated server

--- src/client/java/dev/rvbsm/fsit/FSitModClient.java
+++ src/client/java/dev/rvbsm/fsit/FSitModClient.java
@@ -4,22 +4,27 @@ import dev.rvbsm.fsit.network.ClientNetworkHandler;
 import dev.rvbsm.fsit.network.packet.ConfigSyncC2SPacket;
 import dev.rvbsm.fsit.network.packet.PoseSyncS2CPacket;
 import dev.rvbsm.fsit.network.packet.RestrictionListSyncS2CPacket;
+import lombok.Setter;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
-import net.minecraft.client.MinecraftClient;
 
 public final class FSitModClient implements ClientModInitializer {
+
+	@Setter
+	private static boolean isConnected = false;
 
 	static void saveConfig() {
 		FSitMod.getConfigManager().saveConfig();
 
-		if (MinecraftClient.getInstance().getServer() != null)
-			ClientPlayNetworking.send(new ConfigSyncC2SPacket(FSitMod.getConfig()));
+		if (isConnected) ClientPlayNetworking.send(new ConfigSyncC2SPacket(FSitMod.getConfig()));
 	}
 
 	@Override
 	public void onInitializeClient() {
 		ClientPlayNetworking.registerGlobalReceiver(RestrictionListSyncS2CPacket.TYPE, ClientNetworkHandler::receiveRestrictionList);
 		ClientPlayNetworking.registerGlobalReceiver(PoseSyncS2CPacket.TYPE, ClientNetworkHandler::receivePoseSync);
+
+		ClientPlayConnectionEvents.DISCONNECT.register((handler, client) -> setConnected(false));
 	}
 }
