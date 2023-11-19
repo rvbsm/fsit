e98b606b677dd94db054c70ccb63f07606524215
Ruslan Mykhailenko <mihailencko.r222@gmail.com>
2023-11-19 15:40:19 +0200

    feat(sit): clean up seats if they reloaded and have no passengers

---
+++ src/main/java/dev/rvbsm/fsit/mixin/AreaEffectCloudEntityMixin.java
@@ -0,0 +1,24 @@
+package dev.rvbsm.fsit.mixin;
+
+import dev.rvbsm.fsit.entity.SeatEntity;
+import net.minecraft.entity.AreaEffectCloudEntity;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(AreaEffectCloudEntity.class)
+public abstract class AreaEffectCloudEntityMixin extends Entity {
+
+	public AreaEffectCloudEntityMixin(EntityType<?> type, World world) {
+		super(type, world);
+	}
+
+	@Inject(method = "tick", at = @At("TAIL"))
+	private void tick$cleanSit(CallbackInfo ci) {
+		if (!this.hasPassengers() && this.getCustomName() == SeatEntity.CUSTOM_NAME) this.discard();
+	}
+}
