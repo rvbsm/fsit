1a4ff7522f53e627d8f6bcf5b368bfc1b1c32ed5
Ruslan Mykhailenko <mihailencko.r222@gmail.com>
2024-04-13 19:28:54 +0300

    feat: swing main hand on sit

--- src/main/java/dev/rvbsm/fsit/mixin/ServerPlayNetworkHandlerMixin.java
+++ src/main/java/dev/rvbsm/fsit/mixin/ServerPlayNetworkHandlerMixin.java
@@ -1,14 +1,22 @@
 package dev.rvbsm.fsit.mixin;
 
+import com.llamalad7.mixinextras.sugar.Local;
+import com.llamalad7.mixinextras.sugar.ref.LocalRef;
 import dev.rvbsm.fsit.event.ClientCommandCallback;
+import dev.rvbsm.fsit.event.PassedUseBlockCallback;
 import net.minecraft.network.packet.c2s.play.ClientCommandC2SPacket;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ServerPlayNetworkHandler.class)
@@ -20,5 +28,16 @@ public abstract class ServerPlayNetworkHandlerMixin {
     @Inject(method = "onClientCommand", at = @At("TAIL"))
     public void onClientCommand(@NotNull ClientCommandC2SPacket packet, CallbackInfo ci) {
         ClientCommandCallback.EVENT.invoker().onClientMode(this.player, packet.getMode());
+    }
+
+    @ModifyVariable(method = "onPlayerInteractBlock", at = @At("STORE"))
+    private ActionResult interactBlock(ActionResult interactionActionResult, @Local ServerWorld world, @Local LocalRef<Hand> handRef, @Local BlockHitResult blockHitResult) {
+        if (handRef.get() == Hand.OFF_HAND && interactionActionResult == ActionResult.PASS) {
+            handRef.set(Hand.MAIN_HAND);
+
+            return PassedUseBlockCallback.EVENT.invoker().interactBlock(player, world, blockHitResult);
+        }
+
+        return interactionActionResult;
     }
 }
