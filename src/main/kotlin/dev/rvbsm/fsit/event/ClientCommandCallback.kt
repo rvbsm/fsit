9a8cd11b12e2a0bfd3bd201cc7f7d73a173a6e43
Ruslan Mykhailenko <mihailencko.r222@gmail.com>
2024-04-18 17:13:51 +0300

    feat: start crawling if player sneaked near one-block gap

--- src/main/kotlin/dev/rvbsm/fsit/event/ClientCommandCallback.kt
+++ src/main/kotlin/dev/rvbsm/fsit/event/ClientCommandCallback.kt
@@ -3,15 +3,17 @@ package dev.rvbsm.fsit.event
 import dev.rvbsm.fsit.entity.Pose
 import dev.rvbsm.fsit.network.getConfig
 import dev.rvbsm.fsit.network.setPose
-import kotlinx.coroutines.*
 import net.fabricmc.fabric.api.event.Event
 import net.fabricmc.fabric.api.event.EventFactory
+import net.minecraft.entity.EntityPose
 import net.minecraft.network.packet.c2s.play.ClientCommandC2SPacket
 import net.minecraft.server.network.ServerPlayerEntity
+import net.minecraft.util.math.Direction
 import java.util.*
+import java.util.concurrent.CompletableFuture
+import java.util.concurrent.TimeUnit
 
-private val scope = CoroutineScope(Dispatchers.IO)
-private val sneaks = mutableMapOf<UUID, Job>()
+private val sneaks = mutableMapOf<UUID, CompletableFuture<Boolean>>()
 
 fun interface ClientCommandCallback {
     fun onClientMode(player: ServerPlayerEntity, mode: ClientCommandC2SPacket.Mode)
@@ -28,20 +30,45 @@ fun interface ClientCommandCallback {
             }
 
         override fun onClientMode(player: ServerPlayerEntity, mode: ClientCommandC2SPacket.Mode) {
-            if (mode != ClientCommandC2SPacket.Mode.RELEASE_SHIFT_KEY) return
-            val config = player.getConfig()
-                .takeIf { it.sitting.onDoubleSneak.enabled && player.pitch > it.sitting.onDoubleSneak.minPitch }
-                ?: return
+            if (mode != ClientCommandC2SPacket.Mode.RELEASE_SHIFT_KEY || !player.isOnGround) return
 
-            if (player.uuid !in sneaks) {
-                sneaks[player.uuid] = scope.launch {
-                    delay(config.sitting.onDoubleSneak.delay)
-                    sneaks.remove(player.uuid)
+            val config = player.getConfig().onDoubleSneak.takeUnless { !it.sitting && !it.crawling } ?: return
+
+            if (player.uuid !in sneaks && player.pitch > config.minPitch) {
+                createSneak(player.uuid, config.delay).thenAcceptAsync {
+                    when {
+                        it && config.crawling && player.isNearGap() -> player.setPose(Pose.Crawling)
+                        it && config.sitting -> player.setPose(Pose.Sitting)
+                    }
                 }
             } else {
-                sneaks.remove(player.uuid)?.cancel()
-                player.setPose(Pose.Sitting)
+                sneaks[player.uuid]?.complete(true)
             }
+        }
+
+        private fun createSneak(uuid: UUID, delay: Long) = sneaks.computeIfAbsent(uuid) {
+            CompletableFuture<Boolean>().completeOnTimeout(false, delay, TimeUnit.MILLISECONDS)
+                .apply { thenRunAsync { sneaks.remove(it) } }
+        }
+
+        private fun ServerPlayerEntity.isNearGap(): Boolean {
+            val crawlingDimensions = this.getDimensions(EntityPose.SWIMMING)
+            val crouchingDimensions = this.getDimensions(EntityPose.CROUCHING)
+
+            val expectEmptyAt = pos.add(
+                Direction.fromRotation(yaw.toDouble()).offsetX * 0.1,
+                0.0,
+                Direction.fromRotation(yaw.toDouble()).offsetZ * 0.1,
+            )
+            val expectFullAt = pos.add(
+                Direction.fromRotation(yaw.toDouble()).offsetX * 0.1,
+                crouchingDimensions.height.toDouble(),
+                Direction.fromRotation(yaw.toDouble()).offsetZ * 0.1,
+            )
+
+            return world.isSpaceEmpty(
+                this, crawlingDimensions.getBoxAt(expectEmptyAt).contract(1.0e-6)
+            ) && !world.isSpaceEmpty(this, crawlingDimensions.getBoxAt(expectFullAt).contract(1.0e-6))
         }
     }
 }
