63c0e92215f848558f9e0804db2c1efed265496a
Ruslan Mykhailenko <mihailencko.r222@gmail.com>
2024-07-04 18:55:44 +0300

    feat(geyser): use armor stand as base for seat entity

--- src/main/kotlin/dev/rvbsm/fsit/entity/SeatEntity.kt
+++ src/main/kotlin/dev/rvbsm/fsit/entity/SeatEntity.kt
@@ -2,30 +2,31 @@ package dev.rvbsm.fsit.entity
 
 import dev.rvbsm.fsit.network.clientVelocity
 import dev.rvbsm.fsit.network.config
-import dev.rvbsm.fsit.util.math.clamp
 import net.minecraft.block.piston.PistonBehavior
-import net.minecraft.entity.*
-import net.minecraft.nbt.NbtCompound
+import net.minecraft.entity.EntityDimensions
+import net.minecraft.entity.EntityPose
+import net.minecraft.entity.LivingEntity
+import net.minecraft.entity.decoration.ArmorStandEntity
 import net.minecraft.server.network.ServerPlayerEntity
 import net.minecraft.text.Text
 import net.minecraft.util.math.Box
 import net.minecraft.util.math.Vec3d
 
-class SeatEntity(private val player: ServerPlayerEntity, pos: Vec3d) : Entity(EntityType.BLOCK_DISPLAY, player.world) {
-    private val config
-        get() = player.config
+class SeatEntity(private val player: ServerPlayerEntity, pos: Vec3d) :
+    ArmorStandEntity(player.world, pos.x, pos.y, pos.z) {
+    private val config get() = player.config
     private val groundCollisionBox
         get() = Box.of(pos, width.toDouble(), 1.0e-6, width.toDouble())
-    private val onGround
+    private val hasGroundCollision
         get() = world.getBlockCollisions(this, groundCollisionBox).any { !it.isEmpty }
 
     init {
-        setPosition(pos)
         calculateDimensions()
 
         isInvisible = true
         isInvulnerable = true
         isCustomNameVisible = false
+        isMarker = true
 
         customName = Text.literal("FSit_SeatEntity")
 
@@ -34,52 +35,21 @@ class SeatEntity(private val player: ServerPlayerEntity, pos: Vec3d) : Entity(En
         }
     }
 
-    //? if <=1.20.4
-    override fun initDataTracker() = Unit
-    //? if >1.20.4
-    /*override fun initDataTracker(builder: net.minecraft.entity.data.DataTracker.Builder) = Unit*/
-
-    override fun readCustomDataFromNbt(nbt: NbtCompound) = Unit
-    override fun writeCustomDataToNbt(nbt: NbtCompound) = Unit
-
     override fun tick() {
         if (!world.isClient && !isRemoved) {
-            if (config.sitting.applyGravity) {
-                tickGravity()
-            }
+            super.tickMovement()
 
-            if (firstPassenger == null) {
-                discard()
-            } else if (!config.sitting.applyGravity && !config.sitting.allowInAir && !onGround) {
+            if (firstPassenger == null || (!config.sitting.allowInAir && hasNoGravity() && !hasGroundCollision)) {
                 discard()
             }
         }
-    }
-
-    private fun tickGravity() {
-        updateWaterState()
-
-        velocity = velocity.clamp(0.003)
-        velocity = velocity.add(0.0, -0.04, 0.0)
-        if (isTouchingWater) {
-            velocity = velocity.multiply(0.8, 0.6, 0.8)
-        }
-
-        if (isOnGround && velocity.horizontalLength() >= 1.8e-5) {
-            val blockSlipperiness = world.getBlockState(velocityAffectingPos).block.slipperiness * 0.9
-
-            velocity = velocity.multiply(blockSlipperiness, 1.0, blockSlipperiness)
-        }
-
-        move(MovementType.SELF, velocity)
-    }
-
-    override fun removePassenger(passenger: Entity) {
-        super.removePassenger(passenger)
     }
 
     // note: height of the boat
-    override fun getDimensions(pose: EntityPose): EntityDimensions = EntityDimensions.fixed(0.6f, 0.5625f)
+    //? if <1.20.6
+    override fun getDimensions(pose: EntityPose): EntityDimensions = player.getDimensions(player.pose)
+    //? if >=1.20.6
+    /*override fun getBaseDimensions(pose: EntityPose): EntityDimensions = player.getDimensions(player.pose)*/
 
     override fun updatePassengerForDismount(passenger: LivingEntity): Vec3d = pos
 
@@ -88,6 +58,10 @@ class SeatEntity(private val player: ServerPlayerEntity, pos: Vec3d) : Entity(En
     override fun hasPlayerRider() = false
 
     override fun shouldSave() = false
+
+    override fun hasNoGravity() = !config.sitting.applyGravity
+
+    override fun canClip() = !this.hasNoGravity()
 
     companion object {
         fun create(player: ServerPlayerEntity, pos: Vec3d) {
