e8dc0f106e3a3e41ee835d7ed55c96c3d53d6315
Ruslan Mykhailenko <mihailencko.r222@gmail.com>
2024-07-09 22:16:09 +0300

    refactor(crawl): send entity spawn packets in a bundle

--- src/main/kotlin/dev/rvbsm/fsit/entity/CrawlEntity.kt
+++ src/main/kotlin/dev/rvbsm/fsit/entity/CrawlEntity.kt
@@ -5,15 +5,14 @@ import dev.rvbsm.fsit.util.literal
 import net.minecraft.entity.Entity
 import net.minecraft.entity.EntityType
 import net.minecraft.nbt.NbtCompound
-import net.minecraft.network.packet.s2c.play.EntitiesDestroyS2CPacket
-import net.minecraft.network.packet.s2c.play.EntityPositionS2CPacket
-import net.minecraft.network.packet.s2c.play.EntitySpawnS2CPacket
-import net.minecraft.network.packet.s2c.play.EntityTrackerUpdateS2CPacket
+import net.minecraft.network.packet.s2c.play.*
 import net.minecraft.server.network.ServerPlayerEntity
+import net.minecraft.util.math.Vec3d
 
 // todo: hide shulker's head somehow 😥
 class CrawlEntity(private val player: ServerPlayerEntity) : Entity(EntityType.SHULKER, player.world) {
-    private val crawlPos get() = player.blockPos.toCenterPos()
+    private val crawlBlockPos get() = player.blockPos.up()
+    private val crawlPos get() = Vec3d.ofBottomCenter(crawlBlockPos)
 
     init {
         setPosition(crawlPos)
@@ -31,7 +30,6 @@ class CrawlEntity(private val player: ServerPlayerEntity) : Entity(EntityType.SH
     //? if >1.20.4
     /*override fun initDataTracker(builder: net.minecraft.entity.data.DataTracker.Builder) = Unit*/
 
-
     override fun readCustomDataFromNbt(nbt: NbtCompound) = Unit
     override fun writeCustomDataToNbt(nbt: NbtCompound) = Unit
 
@@ -40,7 +38,7 @@ class CrawlEntity(private val player: ServerPlayerEntity) : Entity(EntityType.SH
     }
 
     override fun tick() {
-        if (blockPos != player.blockPos.up() || age % 10 == 0) {
+        if (blockPos != crawlBlockPos && age % 10 == 0) {
             setPosition(crawlPos)
             player.networkHandler.sendPacket(EntityPositionS2CPacket(this))
         }
@@ -50,10 +48,12 @@ class CrawlEntity(private val player: ServerPlayerEntity) : Entity(EntityType.SH
         fun create(player: ServerPlayerEntity) {
             val crawlEntity = CrawlEntity(player)
 
-            player.networkHandler.sendPacket(EntitySpawnS2CPacket(crawlEntity, 0, crawlEntity.blockPos))
             player.networkHandler.sendPacket(
-                EntityTrackerUpdateS2CPacket(
-                    crawlEntity.id, crawlEntity.dataTracker.dirtyEntries
+                BundleS2CPacket(
+                    setOf(
+                        EntitySpawnS2CPacket(crawlEntity, 0, player.blockPos.up()),
+                        EntityTrackerUpdateS2CPacket(crawlEntity.id, crawlEntity.dataTracker.dirtyEntries ?: listOf()),
+                    )
                 )
             )
 
