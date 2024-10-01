650c45a3846c653bc4d9883021d7357fc7734ad0
Ruslan Mykhailenko <mihailencko.r222@gmail.com>
2024-10-01 13:58:19 +0300

    build(stonecutter): use the least version as versioned project names

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,12 +2,18 @@ import dev.kikugie.stonecutter.StonecutterSettings
 
 rootProject.name = "fsit"
 
+private val minecraftTargets = arrayOf(
+    "1.20" to "1.20.1",
+    "1.20.2" to "1.20.4",
+    "1.20.5" to "1.20.6",
+    "1.21" to "1.21.1",
+)
+
 pluginManagement {
     repositories {
         gradlePluginPortal()
         maven("https://maven.fabricmc.net")
         maven("https://maven.kikugie.dev/releases")
-        maven("https://maven.kikugie.dev/snapshots")
     }
 }
 
@@ -19,7 +25,9 @@ extensions.configure<StonecutterSettings> {
     kotlinController = true
     centralScript = "build.gradle.kts"
     shared {
-        versions("1.20.1", "1.20.4", "1.20.6", "1.21")
+        minecraftTargets.forEach {
+            vers(it.first, it.second)
+        }
     }
     create(rootProject)
 }
--- stonecutter.gradle.kts
+++ stonecutter.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("dev.kikugie.stonecutter")
     alias(libs.plugins.fabric.loom) apply false
 }
-stonecutter active "1.20.1" /* [SC] DO NOT EDIT */
+stonecutter active "1.20" /* [SC] DO NOT EDIT */
 
 stonecutter registerChiseled tasks.register("chiseledBuild", stonecutter.chiseled) {
     group = "project"
