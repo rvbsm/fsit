5ced922e38ac1297eb897d4850c90820c060d9a3
Ruslan Mykhailenko <mihailencko.r222@gmail.com>
2025-01-02 23:57:58 +0200

    fix(seat): funny fix for funny burial bug

--- src/main/kotlin/dev/rvbsm/fsit/event/UpdatePoseListener.kt
+++ src/main/kotlin/dev/rvbsm/fsit/event/UpdatePoseListener.kt
@@ -12,8 +12,16 @@ import dev.rvbsm.fsit.networking.removeCrawl
 import dev.rvbsm.fsit.networking.resetPose
 import dev.rvbsm.fsit.networking.trySend
 import dev.rvbsm.fsit.util.math.centered
+import net.minecraft.util.math.BlockPos
+import net.minecraft.util.math.Direction
+import net.minecraft.util.math.Vec3d
+import net.minecraft.world.BlockView
 
 val UpdatePoseListener = UpdatePoseCallback update@{ player, pose, pos ->
+    if (pose != ModPose.Standing && player.isSneaking) {
+        return@update player.resetPose()
+    }
+
     when (pose) {
         ModPose.Standing -> {
             if (player.vehicle is SeatEntity) player.stopRiding()
@@ -28,7 +36,7 @@ val UpdatePoseListener = UpdatePoseCallback update@{ player, pose, pos ->
             val seatPos = (pos ?: player.pos).let {
                 if (player.config.sitting.shouldCenter) it.centered()
                 else it
-            }
+            }.optimizeHeight(player.world)
 
             SeatEntity.create(player, seatPos)
         }
@@ -41,4 +49,16 @@ val UpdatePoseListener = UpdatePoseCallback update@{ player, pose, pos ->
     }
 
     player.trySend(PoseUpdateS2CPayload(pose, pos ?: player.pos))
+}
+
+/**
+ * spamming sit creation buries player :skull:
+ */
+private fun Vec3d.optimizeHeight(world: BlockView): Vec3d {
+    val blockPos = BlockPos.ofFloored(this)
+    val blockState = world.getBlockState(blockPos)
+    val blockCollisions = blockState.getCollisionShape(world, blockPos)
+    val blockHeight = blockCollisions.getMax(Direction.Axis.Y).coerceIn(1.0, 2.0)
+
+    return add(0.0, blockHeight - 1.0, 0.0)
 }
