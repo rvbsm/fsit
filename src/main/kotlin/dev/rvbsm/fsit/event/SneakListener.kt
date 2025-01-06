338940ad29eb7084c1772058b216703c288781a3
Ruslan Mykhailenko <mihailencko.r222@gmail.com>
2025-01-06 14:19:06 +0200

    refactor: remove yaw to horizontal direction utility method

--- src/main/kotlin/dev/rvbsm/fsit/event/SneakListener.kt
+++ src/main/kotlin/dev/rvbsm/fsit/event/SneakListener.kt
@@ -5,11 +5,13 @@ import dev.rvbsm.fsit.entity.ModPose
 import dev.rvbsm.fsit.networking.config
 import dev.rvbsm.fsit.networking.lastSneakTime
 import dev.rvbsm.fsit.networking.modPose
-import dev.rvbsm.fsit.util.math.toHorizontalDirection
 import net.minecraft.entity.EntityPose
 import net.minecraft.network.packet.c2s.play.ClientCommandC2SPacket
 import net.minecraft.server.network.ServerPlayerEntity
 import net.minecraft.util.Util
+import kotlin.math.PI
+import kotlin.math.cos
+import kotlin.math.sin
 
 val SneakListener = ClientCommandCallback { player, mode ->
     if (mode == ClientCommandC2SPacket.Mode.RELEASE_SHIFT_KEY) {
@@ -28,16 +30,12 @@ private fun ServerPlayerEntity.isNearGap(): Boolean {
     val crawlingDimensions = this.getDimensions(EntityPose.SWIMMING)
     val crouchingDimensions = this.getDimensions(EntityPose.CROUCHING)
 
-    val expectEmptyAt = pos.add(
-        yaw.toHorizontalDirection().offsetX * 0.1,
-        0.0,
-        yaw.toHorizontalDirection().offsetZ * 0.1,
-    )
-    val expectFullAt = pos.add(
-        yaw.toHorizontalDirection().offsetX * 0.1,
-        crouchingDimensions.height.toDouble(),
-        yaw.toHorizontalDirection().offsetZ * 0.1,
-    )
+    val yawRadians = yaw / 180 * PI
+    val offsetX = -sin(yawRadians) * 0.1
+    val offsetZ = cos(yawRadians) * 0.1
+
+    val expectEmptyAt = pos.add(offsetX,0.0, offsetZ)
+    val expectFullAt = pos.add(offsetX, crouchingDimensions.height.toDouble(), offsetZ)
 
     return world.isSpaceEmpty(this, crawlingDimensions.getBoxAt(expectEmptyAt).contract(1.0e-6)) &&
             !world.isSpaceEmpty(this, crawlingDimensions.getBoxAt(expectFullAt).contract(1.0e-6))
--- src/main/kotlin/dev/rvbsm/fsit/util/math/DirectionExt.kt
+++
@@ -1,9 +0,0 @@
-package dev.rvbsm.fsit.util.math
-
-import net.minecraft.util.math.Direction
-import kotlin.math.floor
-
-private val directions = arrayOf(Direction.SOUTH, Direction.WEST, Direction.NORTH, Direction.EAST)
-
-// todo: this is temporary workaround for 1.21.4, should be changed
-fun Float.toHorizontalDirection() = directions[floor(this / 90.0 + 0.5).toInt() and directions.lastIndex]
