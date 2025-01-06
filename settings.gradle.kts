cbb1ec3b19fb831c31b40178eb68f5107ae7b24d
Ruslan Mykhailenko <mihailencko.r222@gmail.com>
2025-01-06 16:54:03 +0200

    chore: bump Stonecutter to 0.5

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,12 +13,12 @@ pluginManagement {
         gradlePluginPortal()
         maven("https://maven.fabricmc.net")
         maven("https://maven.kikugie.dev/releases")
-        maven("https://maven.kikugie.dev/snapshots")
+        //maven("https://maven.kikugie.dev/snapshots")
     }
 }
 
 plugins {
-    id("dev.kikugie.stonecutter") version "0.5-beta.5"
+    id("dev.kikugie.stonecutter") version "0.5"
 }
 
 stonecutter {
