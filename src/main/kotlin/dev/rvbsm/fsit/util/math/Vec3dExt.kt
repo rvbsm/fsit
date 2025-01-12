87e2edecfd1018f3a5e12e7e5bc61c94c4d5dfee
Ruslan Mykhailenko <mihailencko.r222@gmail.com>
2025-01-12 21:19:56 +0200

    fix(seat): wrong centering

--- src/main/kotlin/dev/rvbsm/fsit/util/math/Vec3dExt.kt
+++ src/main/kotlin/dev/rvbsm/fsit/util/math/Vec3dExt.kt
@@ -2,10 +2,11 @@ package dev.rvbsm.fsit.util.math
 
 import net.minecraft.util.math.Vec3d
 import net.minecraft.util.math.Vec3i
+import kotlin.math.floor
 
 
 operator fun Vec3d.plus(vec: Vec3d): Vec3d = add(vec)
 operator fun Vec3d.plus(vec: Vec3i): Vec3d = Vec3d.add(vec, x, y, z)
 operator fun Vec3d.times(value: Double): Vec3d = multiply(value)
 
-fun Vec3d.centered() = Vec3d(x.toInt() + 0.5, y, z.toInt() + 0.5)
+fun Vec3d.centered() = Vec3d(floor(x) + 0.5, y, floor(z) + 0.5)
