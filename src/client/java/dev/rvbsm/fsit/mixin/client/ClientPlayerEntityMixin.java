a5872e7a2c8236cbd8831f9df0af92709dd4ef69
Ruslan Mykhailenko <mihailencko.r222@gmail.com>
2025-06-29 13:33:32 +0300

    fix: wrap entire `ClientPlayerEntity` mixin in stonecutter comment

--- src/client/java/dev/rvbsm/fsit/mixin/client/ClientPlayerEntityMixin.java
+++ src/client/java/dev/rvbsm/fsit/mixin/client/ClientPlayerEntityMixin.java
@@ -1,5 +1,7 @@
 package dev.rvbsm.fsit.mixin.client;
 
+//? if <=1.20.1 {
+/*
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.math.BlockPos;
@@ -15,10 +17,18 @@ public abstract class ClientPlayerEntityMixin extends PlayerEntity {
         super(world, pos, yaw, gameProfile);
     }
 
-    //? if <=1.20.1 {
     @Override
     public double getMountedHeightOffset() {
         return this.getHeight();
     }
-    //?}
+}
+*///?} else {
+
+import net.minecraft.client.network.ClientPlayerEntity;
+
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(ClientPlayerEntity.class)
+public abstract class ClientPlayerEntityMixin {
+
 }
